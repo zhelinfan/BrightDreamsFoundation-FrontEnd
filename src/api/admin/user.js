0cd99b2b66cb935ac33bac4f5cb38e14b7862ad3
Pews Lee <978221138@qq.com>
2023-11-01 08:46:17 +0800

    Finished total user management - frontend.

---
+++ src/api/admin/user.js
@@ -0,0 +1,53 @@
+/*
+用户管理相关的API请求函数
+*/
+import request from '@/utils/request'
+
+const api_name = '/user'
+
+export default {
+
+  /*
+  获取用户分页列表(带搜索)
+  */
+  getPageList(page, limit, searchObj) {
+    return request({
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get',
+      params: searchObj
+    })
+  },
+  removeById(id) {
+    return request({
+      url: `${api_name}/remove/${id}`,
+      method: 'delete'
+    })
+  },
+  save(role) {
+    return request({
+      url: `${api_name}/save`,
+      method: 'post',
+      data: role
+    })
+  },
+  getById(id) {
+    return request({
+      url: `${api_name}/get/${id}`,
+      method: 'get'
+    })
+  },
+  updateById(role) {
+    return request({
+      url: `${api_name}/update`,
+      method: 'put',
+      data: role
+    })
+  },
+  batchRemove(idList) {
+    return request({
+      url: `${api_name}/batchRemove`,
+      method: `delete`,
+      data: idList
+    })
+  }
+}
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">欢迎来到明光筑梦项目！</div>
+    <div>这个项目纯粹是啥呗</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
