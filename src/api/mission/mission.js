39f0cb141392a7b8758ad97dff211bb15e33a779
Ann <xieruolan20030616@126.com>
2023-11-08 20:24:13 +0800

    new functions(商品详情）

---
+++ src/api/mission/mission.js
@@ -0,0 +1,50 @@
+import request from '@/utils/request'
+
+const api_name = '/mission'
+
+export default {
+
+  /*
+  获取mission分页列表(带搜索)
+  */
+  getPageList(page, limit, searchObj) {
+    return request({
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get',
+      params: searchObj
+    })
+  },
+  batchRemove(idList) {
+    return request({
+      url: `${api_name}/batchRemove`,
+      method: `delete`,
+      data: idList
+    })
+  },
+  releaseMission(id) {
+    return request({
+      url: `${api_name}/releaseMission/${id}`,
+      method: 'post'
+    })
+  },
+  removeById(id) {
+    return request({
+      url: `${api_name}/remove/${id}`,
+      method: 'delete'
+    })
+  },
+  save(mission) {
+    return request({
+      url: `${api_name}/save`,
+      method: 'post',
+      data: mission
+    })
+  },
+  upload(file) {
+    return request({
+      url: `${api_name}/upload`,
+      method: 'post',
+      data: file
+    })
+  }
+}
